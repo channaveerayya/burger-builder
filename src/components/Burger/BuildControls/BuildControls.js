e6d9fdbb707378e0c2e5f52bfd72b0ef8113378a
channaveerayya <channucs0009@gmail.com>
2019-10-31 16:33:43 +0530

    removing inf=gredients safely

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from './BuildControls.module.css'
 import BuildControl from './BuildControl/BuildControl'
+import { resolvePreset } from "@babel/core";
 
 const controls = [
   { label: "Salad", type: "salad" },
@@ -14,7 +15,13 @@ const BuildControls = props => {
   return (
     <div className={styles.BuildControls}>
       {controls.map(ctrl => (
-        <BuildControl key={ctrl.label} label={ctrl.label} />
+        <BuildControl
+          key={ctrl.label}
+          label={ctrl.label}
+          added={() => props.ingredientAdded(ctrl.type)}
+          removed={() => { props.ingredientRemoved(ctrl.type) }}
+          disabled={props.disabled[ctrl.type]}
+        />
       ))}
     </div>
   );
