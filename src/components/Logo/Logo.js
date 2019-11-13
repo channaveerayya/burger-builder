b186c6ca78e6b002da790030209989f1da626b5f
channaveerayya <channucs0009@gmail.com>
2019-11-13 23:49:12 +0530

    Using a Logo in our Application

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Logo.module.css'
+import BurgerLogo from '../assets/images/burger-logo.png'
+const Logo = (props) => {
+    return (
+        <div className={styles.Logo}>
+            <img src={BurgerLogo} alt="Logo"/>
+        </div>
+    )
+}
+
+export default Logo
