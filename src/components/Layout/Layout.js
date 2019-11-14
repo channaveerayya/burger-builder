9fb6923394829f9129953515c9469ce3fa7ce5d8
channaveerayya <channucs0009@gmail.com>
2019-11-14 13:23:43 +0530

    Working on Responsive Adjustments

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Aux from '../../hoc/Aux-Hoc'
 import styles from "./layout.module.css";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 const layout = props => (
   <Aux>
-    <Toolbar/>
+    <Toolbar />
+    <SideDrawer />
     <main className={styles.Content}>{props.children}</main>
   </Aux>
 );
