3b655c2b4ecb3e72e05f56ebf7def2a0f3cacd59
channaveerayya <channucs0009@gmail.com>
2019-11-27 16:14:28 +0530

    Guarding Routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {Route,Switch,withRouter} from 'react-router-dom'
+import {Route,Switch,withRouter,Redirect} from 'react-router-dom'
 import Layout from "./hoc/Layout/Layout";
 import  BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from "./containers/Checkout/Checkout";
@@ -13,26 +13,44 @@ class App extends Component {
     this.props.onTryAutoSignup();
   }
   render() {
-    return (
-      <div>
-        <Layout>
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to="/" />
+      </Switch>
+    );
+   
+      if(this.props.isAuth) {
+        routes = (
           <Switch>
             <Route path="/checkout" component={Checkout} />
             <Route path="/orders" component={Orders} />
-            <Route path="/auth" component={Auth} />
             <Route path="/logout" component={Logout} />
             <Route path="/" exact component={BurgerBuilder} />
+            <Redirect to="/" />
           </Switch>
+        );
+      }
+    
+    return (
+      <div>
+        <Layout>
+            {routes}
         </Layout>
       </div>
     );
   }
 }
-
+const mapStateToProps = state => {
+  return {
+    isAuth: state.auth.token !== null
+  };
+};
 const mapDispatchToProps = dispatch =>{
   return {
     onTryAutoSignup:()=>dispatch(ACTIONS.authCheckState())
   }
 }
 
-export default withRouter(connect(null,mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
